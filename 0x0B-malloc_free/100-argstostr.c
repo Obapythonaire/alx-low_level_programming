cd58b98a78f3194944fa175d7907e105369dd7b2
Obapythonaire <99307799+Obapythonaire@users.noreply.github.com>
2022-04-04 16:45:41 +0100

    Fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,36 +10,37 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
-	char *str;
+	char *ar, *str;
+	int i, j, cont;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
-	}
-
-	str = malloc(sizeof(char) * (len + 1));
-
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		j = 0;
+		while (av[i][j] != '\0')
 		{
-			str[k] = av[i][j];
-			k++;
+			j++;
+			cont++;
 		}
-		str[k] = '\n';
-		k++;
+		cont++;
 	}
-
+	cont += 1;
+	ar = malloc(cont * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
+	str = ar;
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			*ar = av[i][j];
+			j++;
+			ar++;
+		}
+		*ar = '\n';
+		ar++;
+	}
 	return (str);
 }
