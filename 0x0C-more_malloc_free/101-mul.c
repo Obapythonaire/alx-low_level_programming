4f622621f77c3a681988eb538ec9618439480350
Obapythonaire <99307799+Obapythonaire@users.noreply.github.com>
2022-04-07 18:34:17 +0100

    fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,14 +1,4 @@
-i#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
-
-/**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
- */
-int is_digit(char *s)
 
 /**
  * main - multiplies two positive numbers
